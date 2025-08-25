f6de8f026eb0c92963419552f7c0077c1529c236
CodewithDeep23 <deepankarsinghvm@gmail.com>
2025-08-25 21:31:39 +0530

    fix: comment out scrollTo function in ClientWrapper and update package dependencies

--- app/ClientWrapper.js
+++ app/ClientWrapper.js
@@ -5,9 +5,12 @@ export default function ClientWrapper({ children }) {
   useEffect(() => {
     if (typeof window !== "undefined") {
 
-      window.scrollTo(0, 0);
-
+      // window.scrollTo(0, 0);
+[]
       if (window.location.hash) {
+
+        history.pushState(null, "", window.location.pathname);
+
         history.replaceState(null, "", window.location.pathname);
       }
     }
